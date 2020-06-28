993a3cffed89b134c31823191997789bd2c2027c
Tim <tja2353@gmail.com>
2020-06-28 11:55:54 +0100

    add Cypress

---
+++ cypress/support/po.ts
@@ -0,0 +1,7 @@
+// we could place this url into cypress.json as "baseUrl"
+const url = 'http://localhost:4200';
+
+export const navigateTo = () => cy.visit(url);
+
+export const getGreeting = () => cy.get('app-root h1');
+// export const getGreeting = () => cy.get('[data-test=title]');
