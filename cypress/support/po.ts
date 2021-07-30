9a619124eed3729e92a74ee21341e8129af9cb78
Tim <tja2353@gmail.com>
2021-07-30 19:37:55 +0100

    increase Cypress defaultCommandTimeout

--- cypress/support/po.ts
+++ cypress/support/po.ts
@@ -3,7 +3,10 @@ const url = 'http://localhost:4200/';
 
 export const navigateTo = () => cy.visit(url);
 
-export const getGreeting = () => cy.get('app-root h1');
+export const getGreeting = () => cy.getBySel('title-text', { timeout: 10000 });
+// export const getGreeting = () => cy.get('app-root h1');
 // export const getGreeting = () => cy.get('[data-test=title]');
 
-export const getHello = () => cy.get('app-root div h1',  { timeout: 10000 });
+export const getHello = () => cy.getBySel('hello-text', { timeout: 10000 });
+
+//     cy.getBySel('hello-text').should('contain', 'Hello');
