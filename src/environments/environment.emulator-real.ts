789bc7bc25c770e076176ef4af370bc72b907b8a
Tim <tja2353@gmail.com>
2022-02-16 16:31:35 +0000

    Tidy environment files

--- src/environments/environment.emulator-real.ts
+++ src/environments/environment.emulator-real.ts
@@ -7,6 +7,18 @@ export const environment: Environment = {
   production: false,
   firebase: {
     config: firebaseConfigDev,
-    emulators: { auth: ['localhost', 9099], firestore: ['localhost', 8080] },
+    emulators: {
+      auth: ['http://localhost:9099'],
+      firestore: ['localhost', 8080],
+    },
   },
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+import 'zone.js/plugins/zone-error'; // Included with Angular CLI.
