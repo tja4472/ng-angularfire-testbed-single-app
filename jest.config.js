bb746762b0d7c8067361fe65b3b6a3050b2796e7
Tim <tja2353@gmail.com>
2022-06-12 18:19:34 +0100

    Add resolver

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,14 @@ module.exports = {
     '^@app\/(.*)$': '<rootDir>/src/app/$1',
     '^#libs\/(.*)$': '<rootDir>/libs/$1/src/public-api.ts'
   },
-  transformIgnorePatterns: ['node_modules/(?!(@angular/fire|@firebase|firebase|rxfire)/)'],
+  resolver: '<rootDir>/jest-resolver.js',   
+  // https://github.com/thymikee/jest-preset-angular/issues/1625
+/*  
+  transformIgnorePatterns: [
+    "node_modules/(?!@angular|@firebase|firebase|@ngrx)",
+    // 'node_modules/(?!.*\\.mjs$)'
+  ],
+*/  
   /*  
   "+lib1":["projects/my-library1/src/public-api.ts"]
   // https://jestjs.io/docs/tutorial-react-native#transformignorepatterns-customization
